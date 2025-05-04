abe73dba53755e0ddee5fb6e605f076aa72014f3
mert <47052769+amldgit@users.noreply.github.com>
2025-05-05 00:26:55 +0200

    consolidation

--- mert_dev_ffnn1.py
+++ mert_dev_ffnn1.py
@@ -2,74 +2,25 @@ import torch
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-import mert_data as dt
 from mert_fnn import FNN
 import torch.nn as nn
 
 window_size = 20
-
-X_train, y_train, X_test, y_test = dt.split_data(train_size=800, sequence_len=window_size, normalize=True)
-# Convert to PyTorch tensors
-X_train_tensor = torch.tensor(X_train) # shape (N, window_size)
-y_train_tensor = torch.tensor(y_train)
-X_test_tensor = torch.tensor(X_test)
-y_test_tensor = torch.tensor(y_test)
+epochs = 100
     
 model = FNN(input_size=window_size, hidden_size=32, hidden_layers=3)
 print(model)
 
-criterion = nn.MSELoss()
+loss_fn = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
-# Initialize lists to store losses
-train_losses = []
-test_losses = []
-best_loss = np.inf
-best_epoch = 0
-epochs = 100
 
-for epoch in range(epochs):
-    # Training
-    model.train()
-    optimizer.zero_grad()
-    output = model(X_train_tensor)
-    loss = criterion(output, y_train_tensor.unsqueeze(1))
-    loss.backward()
-    optimizer.step()
-    
-    # Store the training loss
-    train_losses.append(loss.item())
-    
-    # Evaluation
-    model.eval()
-    with torch.no_grad():
-        test_output = model(X_test_tensor)
-        test_loss = criterion(test_output, y_test_tensor.unsqueeze(1))
-        test_losses.append(test_loss.item())
-        
-    # Check for improvement
-    if test_loss < best_loss:
-        best_loss = test_loss
-        best_epoch = epoch
-        # Save the model state
-        torch.save(model.state_dict(), 'fnn_best_model.pt')
-    
-    if (epoch+1) % 10 == 0:
-        print(f"Epoch {epoch+1}, Train Loss: {loss.item():.4f}, Test Loss: {test_loss.item():.4f}")
-
-model.eval()
-with torch.no_grad():
-    predictions = model(X_test_tensor)
-    final_test_loss = criterion(predictions, y_test_tensor.unsqueeze(1))
-    print(f"Final Test MSE: {final_test_loss.item():.4f}")
+final_test_loss, y_test_np, predictions_np = model.perform_training_no_batch(loss_fn, optimizer, epochs=epochs, train_size=800, sequence_len=window_size, normalize=True)
+train_losses = np.array(model.train_losses)
+test_losses = np.array(model.test_losses)
 
 # Plot training and test losses
-#convert to original scale
-train_losses = np.array(train_losses)
-test_losses = np.array(test_losses)
 
-#train_losses = np.sqrt(train_losses) * 255.0
-#test_losses = np.sqrt(test_losses) * 255.0
 num_epochs = len(train_losses)
 plt.figure(figsize=(10, 6))
 plt.plot(range(1, num_epochs + 1), train_losses, label='Training Loss')
@@ -81,19 +32,6 @@ plt.title('Training and Test Losses Over Time')
 plt.legend()
 plt.grid(True)
 plt.show()
-
-model.load_state_dict(torch.load('fnn_best_model.pt'))
-model.eval()
-
-# Generate predictions with the loaded model
-with torch.no_grad():
-    predictions = model(X_test_tensor)
-
-# Convert tensors to numpy for plotting
-y_test_np = y_test_tensor.numpy()
-#y_yest_np = np.sqrt(y_test_np)*255.0
-predictions_np = predictions.numpy() 
-#predictions_np = np.sqrt(predictions_np)*255.0
 
 import matplotlib.pyplot as plt
 plt.plot(predictions_np[100:], label='Predicted')
--- mert_fnn.py
+++ mert_fnn.py
@@ -1,4 +1,7 @@
+import numpy as np
+import torch
 import torch.nn as nn
+import mert_data as dt
 
 class FNN(nn.Module):
     def __init__(self, input_size, hidden_size=32, hidden_layers=1):
@@ -36,3 +39,60 @@ class FNN(nn.Module):
     
     def forward(self, x):
         return self.layers(x)
+    
+    path_best = 'fnn_best_model.pt'
+    train_losses = []
+    test_losses = []
+    best_loss = np.inf
+    best_epoch = 0
+    final_test_loss = np.inf
+    
+    def perform_training_no_batch(self,loss_fn, optimizer, epochs=100, train_size=800, sequence_len=5, normalize=True):    
+        X_train, y_train, X_test, y_test = dt.split_data(train_size=train_size, sequence_len=sequence_len, normalize=normalize) 
+           
+        # Convert to PyTorch tensors
+        X_train = torch.tensor(X_train) # shape (N, window_size)
+        y_train = torch.tensor(y_train)
+        X_test = torch.tensor(X_test)
+        y_test = torch.tensor(y_test)
+        
+        for epoch in range(epochs):
+            self.train()
+            optimizer.zero_grad()
+            output = self(X_train)
+            loss = loss_fn(output, y_train.unsqueeze(1))
+            loss.backward()
+            optimizer.step()
+                    
+            # Store the training loss
+            self.train_losses.append(loss.item())
+            
+            # Test the model for epoch
+            self.eval()
+            with torch.no_grad():
+                test_output = self(X_test)
+                test_loss = loss_fn(test_output, y_test.unsqueeze(1))
+                self.test_losses.append(test_loss.item())                
+            
+            if test_loss < self.best_loss:
+                self.best_loss = test_loss
+                self.best_epoch = epoch
+                # Save the model state
+                torch.save(self.state_dict(), self.path_best)
+            
+            if (epoch+1) % 10 == 0:
+                print(f"Epoch {epoch+1}, Train Loss: {loss.item():.4f}, Test Loss: {test_loss.item():.4f}")
+                
+        #load the best model
+        self.load_state_dict(torch.load('fnn_best_model.pt'))
+        self.eval()
+        with torch.no_grad():
+            predictions = self(X_test)
+            self.final_test_loss = loss_fn(predictions, y_test.unsqueeze(1))
+            print(f"Final Test MSE: {self.final_test_loss.item():.4f}")
+
+        # Convert tensors to numpy for plotting
+        y_test_np = y_test.numpy()        
+        predictions_np = predictions.numpy() 
+        return self.final_test_loss, y_test_np, predictions_np
+            
